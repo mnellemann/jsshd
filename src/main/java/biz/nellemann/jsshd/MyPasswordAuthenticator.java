f06dc9c7f7d434f119ee7b49ebaa71e71be5f5f7
Mark Nellemann <mark.nellemann@gmail.com>
2023-12-21 14:15:39 +0100

    Log authentication.

--- src/main/java/biz/nellemann/jsshd/MyPasswordAuthenticator.java
+++ src/main/java/biz/nellemann/jsshd/MyPasswordAuthenticator.java
@@ -4,8 +4,13 @@ import org.apache.sshd.server.auth.AsyncAuthException;
 import org.apache.sshd.server.auth.password.PasswordAuthenticator;
 import org.apache.sshd.server.auth.password.PasswordChangeRequiredException;
 import org.apache.sshd.server.session.ServerSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MyPasswordAuthenticator implements PasswordAuthenticator {
+
+    private final static Logger log = LoggerFactory.getLogger(MyPasswordAuthenticator.class);
+
 
     private final String user;
     private final String pass;
@@ -17,6 +22,7 @@ public class MyPasswordAuthenticator implements PasswordAuthenticator {
 
     @Override
     public boolean authenticate(String username, String password, ServerSession session) throws PasswordChangeRequiredException, AsyncAuthException {
+        log.info("authenticate() - username: {}, password: {}", username, password);
         return username.equals(this.user) && password.equals(this.pass);
     }
 
